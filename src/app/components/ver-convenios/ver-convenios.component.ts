53d5fbd2915051a10d43ff0c70a3f5dc100602c6
Olmes Zea <holmeszea@gmail.com>
2021-11-30 23:01:28 -0500

    listo despliegue empresas vista inicial

--- src/app/components/ver-convenios/ver-convenios.component.ts
+++ src/app/components/ver-convenios/ver-convenios.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { EmpresasModel } from 'src/app/models/empresas';
+import { EmpresasService } from 'src/app/services/empresas/empresas.service';
+//import { TablaEmpresasComponent} from '../tabla-empresas/tabla-empresas.component';
 
 @Component({
   selector: 'app-ver-convenios',
   templateUrl: './ver-convenios.component.html',
   styleUrls: ['./ver-convenios.component.scss']
 })
+
 export class VerConveniosComponent implements OnInit {
 
-  constructor() { }
+  public convenios: EmpresasModel[]=[];
 
-  ngOnInit(): void {
+  constructor(private empresasService: EmpresasService, private router: Router ) { }
+
+  async ngOnInit(): Promise <void> {
+    this.convenios = await this.obtenerEmpresas();
+  }
+
+  public async obtenerEmpresas(): Promise<any>{
+    try{
+      const response =await this.empresasService.obtenerEmpesas();
+      return response.datos;
+    }catch(error){
+      this.router.navigate(['/error']);
+      console.log(error);
+    }
   }
 
 }
+
+
+
+
